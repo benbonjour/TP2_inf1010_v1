4d0de030dab23cbb0047a6d45be6a406869adb92
Benjamin Brodeur Mathieu <benjaminbrodeurmathieu@Benjamin-Brodeur-Mathieus-MacBook-Pro.local>
2013-02-06 20:08:59 -0500

    Initial Commit

---
+++ TP2_inf1010_v1/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  TP2_inf1010_v1
+//
+//  Created by Benjamin Brodeur Mathieu on 2013-02-06.
+//  Copyright (c) 2013 Benjamin Brodeur Mathieu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
