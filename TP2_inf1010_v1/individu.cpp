d2ee021d88a1f2a154e4301d48f5f822a79710c7
Benjamin Brodeur Mathieu <benjaminbrodeur@gmail.com>
2013-02-07 11:19:26 -0500

    Voici trois en-têtes de fait :D
    voila :D

--- TP2_inf1010_v1/individu.cpp
+++ TP2_inf1010_v1/individu.cpp
@@ -6,4 +6,98 @@
  * Date de modification     : 
  ************************************************************************************/
 
+#include <string>
 
+using namespace std;
+
+#ifndef _INDIVIDU_H_
+#define _INDIVIDU_H_
+
+class Individu
+{
+public:
+    
+    //Constructeurs
+    
+    /************************************************************************************
+     * Description              : Individu() : constructeur par defaut initialise les at
+     *                            tributs de l'individu par des valeurs par defaults
+     * Parametre                : AUCUN
+     * Valeur de retour         : AUCUNE
+     * Remarque                 : AUCUNE
+     ************************************************************************************/
+    Individu()
+    {
+    
+    }
+    
+    
+    /************************************************************************************
+     * Description              : Individu(string nom, string prenom, string* listeVins
+     *                            Preferes) : constructeur par parametres qui initialise 
+     *                            les attributs de l'individu par des valeurs par defaults
+     * Parametre                : String:nom "nom de l'individu, String:prenom "prenom de
+     *                            l'individu", String*:listeVinsPreferes "tableau de stri
+     *                            ng contenant la liste de vin preferes de l'individu"
+     * Valeur de retour         : AUCUNE
+     * Remarque                 : AUCUNE
+     ************************************************************************************/
+    Individu(string nom, string prenom, string* listeVinsPreferes)
+    {
+    
+    }
+    
+    /************************************************************************************
+     * Description              : Individu(const Individu& unIndividu) : constructeur par 
+     *                            defaut initialise les at
+     *                            tributs de l'individu par des valeurs par defaults
+     * Parametre                : AUCUN
+     * Valeur de retour         : AUCUNE
+     * Remarque                 : AUCUNE
+     ************************************************************************************/
+    Individu(const Individu& unIndividu)
+    {
+    
+    }
+    
+    
+    
+    
+    
+    //Destructeurs
+    ~Individu();
+    
+    //Modifiers & accesors
+    string getNom() const;
+    string getPrenom() const;
+    void setNom(string nom);
+    void setPrenom(string prenom);
+    
+    //Surcharge d'operateurs
+    bool operator==(Individu& unIndividu) const;
+    Individu& operator=(const Individu& unIndividu) const;
+    
+    //Fonction add et del Pour listeVinsPreferes
+    void add(string nomDuVin);
+    void del(string nomDuVin);
+    
+    //Fonction getPrix pour prixEntree
+    double getPrix() const;
+    
+    //Methode getString()
+    
+    string getString() const;
+    
+    
+protected:
+    
+    double prixEntree;
+    
+private:
+    
+    string nom_;
+    string prenom_;
+    string* listeVinsPreferes_;
+};
+
+#endif // _INDIVIDU_H_
